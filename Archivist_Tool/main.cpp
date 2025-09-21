b2478d293be0a85be1fea9f5caa1e9ea84b91eab
zera2ul <mihail.bereza.ict@gmail.com>
2025-09-21 16:14:29 +0300

    Add unpack context menu command

--- Archivist_Tool/main.cpp
+++ Archivist_Tool/main.cpp
@@ -23,15 +23,23 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+
 	if (argc == 1)
 	{
 		App app;
 		return app.run();
 	}
-	else if (argc == 2)
+	else if (argc == 3)
 	{
-		FileFolderManager file_folder_manager;
-		file_folder_manager.writePath(argv[1]);
+		if (string(argv[1]) == "pack")
+		{
+			FileFolderManager file_folder_manager;
+			file_folder_manager.writePath(argv[2]);
+		}
+		else if (string(argv[1]) == "unpack")
+			Archiver archiver(argv[2]);
+
 		return 0;
 	}
 }
